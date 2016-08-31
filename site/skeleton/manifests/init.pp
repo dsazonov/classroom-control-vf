a877bc7fe06ad16f6572d5aa1e78bed39ae7da6e
dsazonov <dsazonov@icloud.com>
2016-08-31 10:36:41 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,11 @@
 class skeleton {
-  file '/etc/skel/.bashrc' {
+  file { '/etc/skel/.bashrc':
       ensure => directory,
       owner => 'root',
       group => 'root',
       source => 'puppet:///modules/skeleton/bashrc',
     }
-    file '/etc/skel' {
+    file { '/etc/skel':
       ensure => file,
       owner => 'root',
       group => 'root',
