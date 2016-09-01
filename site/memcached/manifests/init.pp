0d0e5fd70741b3b78d65ec86f42441c829787939
dsazonov <dsazonov@icloud.com>
2016-09-01 06:30:50 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+class memcached {
+  package { 'memcached':
+    ensure => latest,
+  }
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['memcached'],
+    notify => Service['memcached'],
+    source => 'puppet:///modules/memcached/memcached',
+  }
+  service { 'memcached':
+    enabled => true,
+    ensure  => running,
+  }
+
+}
