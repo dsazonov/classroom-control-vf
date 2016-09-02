edbb93578ea0e811af3a76cd25bfcc993385b7e1
dsazonov <dsazonov@icloud.com>
2016-09-02 09:37:05 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -45,25 +45,25 @@ $docroot = $root? {
 
   file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
-    require Package['nginx'],
+    require => Package['nginx'],
   }
 
   file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
-    require Package['nginx'],
+    require => Package['nginx'],
   }
   file { "${confdir}/nginx.conf":
     ensure  => file,
     content => template('nginx/nginx.conf.erb'),
     notify  => Service['nginx'],
-    require Package['nginx'],
+    require => Package['nginx'],
   }
   file { "${confdir}/conf.d/default.conf":
     ensure  => file,
     content => template('nginx/default.conf.erb'),
     notify  => Service['nginx'],
-    require Package['nginx'],
+    require => Package['nginx'],
   }
 
   service { 'nginx':
