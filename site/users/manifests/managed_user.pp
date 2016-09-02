953cec73308e1c6b0476095d98ba65812b786911
dsazonov <dsazonov@icloud.com>
2016-09-02 05:23:11 -0400

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,12 @@
+defineusers::managed_user (
+  $group = $title,
+) {
+user { $title:
+  ensure => present,
+  }
+file { "/home/${title}":
+  ensure => directory,
+  owner  => $title,
+  group  => $group,
+  }
+}
